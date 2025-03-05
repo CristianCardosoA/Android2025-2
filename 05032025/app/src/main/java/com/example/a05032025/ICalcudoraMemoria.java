ef576c0336da6b853cd694e4b76d42f54d30f9bf
unknown <cc@git>
2025-03-05 09:02:12 -0600

    Mis intents con tres actividades xoxo

---
+++ 05032025/app/src/main/java/com/example/a05032025/ICalcudoraMemoria.java
@@ -0,0 +1,10 @@
+package com.example.a05032025;
+
+public interface ICalcudoraMemoria {
+
+    String concat(String numero);
+
+    String concat(Operacion operacion);
+
+    void clear();
+}
--- Calculadora2025/app/src/main/java/com/example/calculadora2025/MainActivity.kt
+++ Calculadora2025/app/src/main/java/com/example/calculadora2025/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculadora2025.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivityBinding : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-    }
-}
+        }  //Fin ViewCompat
+
+        var resultado = 0.0 // creo una nueva variable quie se llama resultado y le asigno el valor de entrada 0.0
+        //Creao cuatro banderas para las operaciones principales. La idea es que cuando un usuario elija una operación, la bandera de dicha operación se prenda
+        var suma = false //Creo la bamdera para la suma y la inicializo como false
+        var resta = false
+        var multiplicacion = false
+        var division = false
+
+        R.id.cero_button
+
+    }   //Fin función OnCreate()
+}  //Fin Clase MainActivity
+
+//Esto es un comentario
+/* comentario multilinea 1
+comentario multilinea 2
+comentario multilinea 3
+comentario multilinea 4
+ */
